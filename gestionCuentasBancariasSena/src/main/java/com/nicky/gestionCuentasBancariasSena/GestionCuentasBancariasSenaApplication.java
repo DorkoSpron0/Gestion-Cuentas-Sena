54be20cd28a520170216bd0e6625e1adddc5fe22
dorkospron0 <tareassec9@gmail.com>
2025-06-18 06:40:37 -0500

    first commit

---
+++ gestionCuentasBancariasSena/src/main/java/com/nicky/gestionCuentasBancariasSena/GestionCuentasBancariasSenaApplication.java
@@ -0,0 +1,13 @@
+package com.nicky.gestionCuentasBancariasSena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCuentasBancariasSenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCuentasBancariasSenaApplication.class, args);
+	}
+
+}
---
+++ gestionCuentasBancariasSena/src/test/java/com/nicky/gestionCuentasBancariasSena/GestionCuentasBancariasSenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicky.gestionCuentasBancariasSena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCuentasBancariasSenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
